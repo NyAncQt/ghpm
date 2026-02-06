2d6a8cf07c7fec44a91e7f9f3793f67904d7397d
Timur <timurka.istanbul@gmail.com>
2026-02-06 17:39:09 +0300

    Update ghpm package manager

--- main.go
+++ main.go
@@ -1,25 +1,67 @@
 package main
 
 import (
+	"encoding/json"
 	"fmt"
 	"os"
 	"os/exec"
 	"path/filepath"
 	"strings"
+	"time"
 )
 
+type Manifest struct {
+	Name        string    `json:"name"`
+	Repo        string    `json:"repo"`
+	URL         string    `json:"url"`
+	InstalledAt time.Time `json:"installed_at"`
+	Commit      string    `json:"commit,omitempty"`
+	Version     string    `json:"version,omitempty"`
+}
+
+var baseDir, packagesDir, manifestsDir string
+
+func initDirs() error {
+	home, err := os.UserHomeDir()
+	if err != nil {
+		return err
+	}
+	baseDir = filepath.Join(home, ".ghpm")
+	packagesDir = filepath.Join(baseDir, "packages")
+	manifestsDir = filepath.Join(baseDir, "manifests")
+
+	return os.MkdirAll(manifestsDir, 0755)
+}
+
 func main() {
-	if len(os.Args) < 3 {
-		fmt.Println("Usage: ghpm install owner/repo")
+	if len(os.Args) < 2 {
+		fmt.Println("Usage: ghpm <command> [args]")
+		fmt.Println("Commands: install, remove, list, update")
+		return
+	}
+
+	if err := initDirs(); err != nil {
+		fmt.Println("Failed to init directories:", err)
 		return
 	}
 
 	command := os.Args[1]
-	target := os.Args[2]
 
 	switch command {
 	case "install":
-		installRepo(target)
+		if len(os.Args) < 3 {
+			fmt.Println("Usage: ghpm install owner/repo")
+			return
+		}
+		installRepo(os.Args[2])
+	case "remove":
+		if len(os.Args) < 3 {
+			fmt.Println("Usage: ghpm remove repo-name")
+			return
+		}
+		removeRepo(os.Args[2])
+	case "list":
+		listRepos()
 	default:
 		fmt.Println("Unknown command:", command)
 	}
@@ -31,35 +73,70 @@ func installRepo(repo string) {
 		return
 	}
 
-	url := "https://github.com/" + repo + ".git"
-
-	home, err := os.UserHomeDir()
-	if err != nil {
-		fmt.Println("Cannot find home directory")
-		return
-	}
-
-	installDir := filepath.Join(home, ".ghpm", "packages")
-	err = os.MkdirAll(installDir, 0755)
-	if err != nil {
-		fmt.Println("Failed to create ghpm directory")
-		return
-	}
-
 	repoName := strings.Split(repo, "/")[1]
-	dest := filepath.Join(installDir, repoName)
+	dest := filepath.Join(packagesDir, repoName)
+	if _, err := os.Stat(dest); err == nil {
+		fmt.Println("Already installed:", repoName)
+		return
+	}
 
+	url := "https://github.com/" + repo + ".git"
 	fmt.Println("Cloning", url, "to", dest)
 
 	cmd := exec.Command("git", "clone", url, dest)
 	cmd.Stdout = os.Stdout
 	cmd.Stderr = os.Stderr
-
-	err = cmd.Run()
-	if err != nil {
+	if err := cmd.Run(); err != nil {
 		fmt.Println("Git clone failed:", err)
 		return
 	}
 
+	manifest := Manifest{
+		Name:        repoName,
+		Repo:        repo,
+		URL:         url,
+		InstalledAt: time.Now(),
+	}
+	saveManifest(manifest)
 	fmt.Println("Installed", repoName)
+}
+
+func removeRepo(name string) {
+	manifestPath := filepath.Join(manifestsDir, name+".json")
+	pkgPath := filepath.Join(packagesDir, name)
+
+	if _, err := os.Stat(pkgPath); os.IsNotExist(err) {
+		fmt.Println("Repo not installed:", name)
+		return
+	}
+
+	os.RemoveAll(pkgPath)
+	os.Remove(manifestPath)
+	fmt.Println("Removed", name)
+}
+
+func listRepos() {
+	files, err := os.ReadDir(manifestsDir)
+	if err != nil {
+		fmt.Println("Failed to read manifests:", err)
+		return
+	}
+
+	if len(files) == 0 {
+		fmt.Println("No installed packages.")
+		return
+	}
+
+	fmt.Println("Installed packages:")
+	for _, f := range files {
+		var m Manifest
+		data, _ := os.ReadFile(filepath.Join(manifestsDir, f.Name()))
+		json.Unmarshal(data, &m)
+		fmt.Printf("- %s (%s)\n", m.Name, m.Repo)
+	}
+}
+
+func saveManifest(m Manifest) {
+	data, _ := json.MarshalIndent(m, "", "  ")
+	os.WriteFile(filepath.Join(manifestsDir, m.Name+".json"), data, 0644)
 }
